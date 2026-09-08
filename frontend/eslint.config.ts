af51660c1fd93773b266f4b25b0019898763525b
Cheltcov Evgenii <chill.icp@gmail.com>
2026-09-08 10:31:53 +0900

    chore: simplify eslint config

--- frontend/eslint.config.ts
+++ frontend/eslint.config.ts
@@ -3,11 +3,6 @@ import { defineConfigWithVueTs, vueTsConfigs } from "@vue/eslint-config-typescri
 import simpleImportSort from "eslint-plugin-simple-import-sort";
 import pluginVue from "eslint-plugin-vue";
 
-// To allow more languages other than `ts` in `.vue` files, uncomment the following lines:
-// import { configureVueProject } from '@vue/eslint-config-typescript'
-// configureVueProject({ scriptLangs: ['ts', 'tsx'] })
-// More info at https://github.com/vuejs/eslint-config-typescript/#advanced-setup
-
 export default defineConfigWithVueTs(
   {
     plugins: {
@@ -15,17 +10,12 @@ export default defineConfigWithVueTs(
     },
   },
   {
-    name: "app/files-to-lint",
-    files: ["**/*.{ts,mts,tsx,vue}"],
+    name: "files-to-lint",
+    files: ["**/*.{ts,vue}"],
   },
   {
     ignores: ["node_modules/**", ".env", ".env.*", "package-lock.json", "**/locales/*", "src/api/**/*"],
   },
-  {
-    name: "app/files-to-ignore",
-    ignores: ["**/dist/**", "**/dist-ssr/**", "**/coverage/**"],
-  },
-
   pluginVue.configs["flat/essential"],
   vueTsConfigs.recommended,
   skipFormatting,
@@ -42,22 +32,6 @@ export default defineConfigWithVueTs(
       "simple-import-sort/imports": "error",
       "simple-import-sort/exports": "error",
       "sort-imports": "off",
-    },
-  },
-  {
-    files: ["src/lib/**/*"],
-    rules: {
-      "no-restricted-imports": [
-        "error",
-        {
-          patterns: [
-            {
-              group: ["@/app/*"],
-              message: "Import @/app/* from @/lib/* is not allowed",
-            },
-          ],
-        },
-      ],
     },
   },
 );
